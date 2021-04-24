a6557d6c46adfcc8060ad1e4e419cc3337cc43a6
Brian Archibald <brianallenarchibald@gmail.com>
2021-04-24 14:15:20 -0500

    Added bootstrap, started word card

--- words/src/App.js
+++ words/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import  Card  from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 function App() {
+
+  function getRandomWord(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+const wordList = ['I', 'see', 'can', 'go', 'a', 'like', 'me', 'the', 'to', 'is']
+const word = getRandomWord(wordList);
+
+  // const completeWordList = [
+  //   ['I', 'see', 'can', 'go', 'a', 'like', 'me', 'the', 'to', 'is'],
+  //   ['am', 'at', 'you', 'he', 'it', 'in', 'my', 'on', 'we', 'up'],
+  //   ['she', 'be', 'and', 'red', 'an', 'no', 'by', 'here', 'will', 'look'],
+  //   ['was', 'day', 'play', 'all', 'got', 'not', 'come', 'so', 'said', 'do']
+  // ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className="my-3 p-3 rounded">
+      <Card.Body>
+          <Card.Title as="div">
+            <strong>Elladie's Words</strong>
+          </Card.Title>
+
+          <Card.Text as="div">
+            <div className="my-3">
+              {word}
+            </div>
+          </Card.Text>
+
+          <Card.Text as="h4">
+            <Button onClick={getRandomWord(wordList)}>
+              <h4>Next Word</h4>
+            </Button>
+          </Card.Text>
+
+      </Card.Body>
+    </Card>
+
   );
 }
 
